floating-point precision issues fixed

xarray_extras/geo_regrid.py
@@ -55,8 +55,8 @@ class geo_regrid:
         if longitude_circular is None and len(longitude) >= 2:
             eps = np.finfo(ds_lon.dtype).eps
             ds_lon_delta = abs(ds_lon[1] - ds_lon[0])
-            longitude_circular = bool(abs(ds_lon_span - 360.) <= 4. * 360. * eps or
-                                      abs(ds_lon_span + ds_lon_delta - 360.) <= 4. * 360. * eps)
+            longitude_circular = bool(abs(ds_lon_span - 360.) <= 8. * 360. * eps or
+                                      abs(ds_lon_span + ds_lon_delta - 360.) <= 8. * 360. * eps)
             logger().debug(f'longitude_circular={longitude_circular} for ds={ds.xrx.short_dataset_repr()}')
 
         # handle overlapping target longitude coordinates if necessary
@@ -64,12 +64,12 @@ class geo_regrid:
         if longitude_circular:
             # remove target longitude coordinate which is overlapping mod 360
             eps = np.finfo(longitude.dtype).eps
-            if abs(abs(longitude[-1] - longitude[0]) - 360.) <= 4. * 360. * eps:
+            if abs(abs(longitude[-1] - longitude[0]) - 360.) <= 8. * 360. * eps:
                 longitude_ori = longitude
                 longitude = longitude[:-1]
             # remove ds' longitude coordinate which is overlapping mod 360
             eps = np.finfo(ds_lon.dtype).eps
-            if abs(ds_lon_span - 360.) <= 4. * 360. * eps:
+            if abs(ds_lon_span - 360.) <= 8. * 360. * eps:
                 ds = ds.isel({lon_label: slice(None, -1)})
 
         # if necessary, align longitude coordinates of ds to the target longitude by normalizing and rolling or sorting

xarray_extras/regrid.py
@@ -16,17 +16,15 @@ def is_coord_regularly_gridded(coord, abs_err=None):
     if n < 2:
         return True
     d_coord = np.diff(coord)
-    if abs_err is not None:
-        return np.all(np.abs(d_coord - d_coord[0]) <= abs_err)
-    try:
-        eps = np.finfo(coord.dtype).eps
-    except ValueError:
-        # dtype not inexact
-        return np.all(d_coord == d_coord[0])
-    # floating-point
-    abs_coord_plus_1 = np.fabs(coord) + 1.
-    rel_err = 8. * eps * np.maximum(abs_coord_plus_1[1:], abs_coord_plus_1[:-1])
-    return np.all(np.fabs(d_coord - d_coord[0]) <= rel_err)
+    if abs_err is None:
+        try:
+            eps = np.finfo(coord.dtype).eps
+        except ValueError:
+            # dtype not inexact
+            return np.all(d_coord == d_coord[0])
+        # floating-point
+        abs_err = 4 * eps * np.max(np.fabs(coord))
+    return np.all(np.fabs(d_coord - d_coord[0]) <= 2 * abs_err)
 
 
 @xr.register_dataset_accessor('regrid')